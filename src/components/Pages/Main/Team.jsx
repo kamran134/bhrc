3e4d966ce35b656ee37ebf6f12ec7c68e9765aa6
Kamran Kazimi <k.kazimi@zirinc.com>
2021-06-27 01:49:48 +0400

    social network

--- src/components/Pages/Main/Team.jsx
+++ src/components/Pages/Main/Team.jsx
@@ -30,9 +30,9 @@ const MainTeam = () => {
                 en: '<p>Rasul Jafarov is a co-founder and Chairman of the Baku Human Rights Club (BHRC), a local NGO actively operating for the promotion and protection of human rights and democratic values in Azerbaijan. To that end, the BHRC monitors situation regarding civil and political freedoms as well as socio-economic rights in the country, conducts investigations into reports of violations and provides necessary legal support, campaigns on cases of concern, and engages in advocacy on the regional and international level.<br/><br/>Rasul was a coordinator of the  “Sing for Democracy” campaign aiming to raise awareness on human rights issues on the eve of the 2012 Eurovision Song Contest held in Baku. The campaign later transformed into “Art for democracy” that uses art and artistic expression for human rights promotion.<br/><br/>Rasul compiled information on political prisoners in Azerbaijan, reporting on his findings to international institutions until he became one of those. He has been arrested for his human rights work in 2014. After spending almost two years behind bars he has been released as a result of efforts conducted by local and international community on his freedom. In 2020, Rasul was fully acquitted by the Supreme Court of Azerbaijan based on the Judgement of the European Court of Human Rights indicating his detention as politically-motivated.<br/><br/>Rasul is the recipient of the Justice Makers Award and the Andrei Sakharov Freedom Award instituted by the International Bridges to Justice and the Norwegian Helsinki Committee respectively.</p>'
             },
             social: {
-                twitter: '',
-                instagram: '',
-                facebook: ''
+                twitter: 'https://twitter.com/rasuljafarov/',
+                instagram: 'https://www.instagram.com/rasuljafarov/',
+                facebook: 'https://www.facebook.com/rasul.jafarov/'
             }
         }
     ];
@@ -50,7 +50,7 @@ const MainTeam = () => {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla imperdiet turpis
                             quis nunc vestibulum auctor. Fusce semper vel enim sit amet ultricies.
                         </div>
-                        <button className='bhrc-btn orange-btn'>Meet us <ImArrowRight2/></button>
+                        {/* <button className='bhrc-btn orange-btn'>Meet us <ImArrowRight2/></button> */}
                     </div>
                     <Members members={membersMock} />
                 </div>
@@ -73,9 +73,9 @@ const Members = ({members}) => {
                         <div className='name'>{member.name[lang]}</div>
                         <div className='profession'>{member.profession[lang]}</div>
                         <div className='social-network'>
-                            <span className='icon'><Twitter/></span>
-                            <span className='icon'><Facebook/></span>
-                            <span className='icon'><Instagram/></span>
+                            <a href={member.social.twitter} target="_blank" rel="noreferrer" className='icon'><Twitter/></a>
+                            <a href={member.social.facebook} target="_blank" rel="noreferrer" className='icon'><Facebook/></a>
+                            <a href={member.social.instagram} target="_blank" rel="noreferrer" className='icon'><Instagram/></a>
                         </div>
                     </div>
                     <Modal
@@ -89,9 +89,9 @@ const Members = ({members}) => {
                         <div className='profession'>{member.profession[lang]}</div>
                         <div className='bio' dangerouslySetInnerHTML={{__html: member.bio[lang]}} />
                         <div className='social-network'>
-                            <span className='icon'><Twitter/></span>
-                            <span className='icon'><Facebook/></span>
-                            <span className='icon'><Instagram/></span>
+                            <a href={member.social.twitter} target="_blank" rel="noreferrer" className='icon'><Twitter/></a>
+                            <a href={member.social.facebook} target="_blank" rel="noreferrer" className='icon'><Facebook/></a>
+                            <a href={member.social.instagram} target="_blank" rel="noreferrer" className='icon'><Instagram/></a>
                         </div>
                     </Modal>
                 </>
